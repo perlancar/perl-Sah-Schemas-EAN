f1a91a7c363c0c21f06eb1574fda6db0db930b26
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-03-04 15:14:17 +0700

    Add examples (tests)

--- lib/Sah/Schema/ean13.pm
+++ lib/Sah/Schema/ean13.pm
@@ -1,6 +1,8 @@
 package Sah::Schema::ean13;
 
+# AUTHORITY
 # DATE
+# DIST
 # VERSION
 
 our $schema = [str => {
@@ -14,6 +16,13 @@ Checksum digit must be valid.
 _
     match => '\A[0-9]{13}\z',
     'x.perl.coerce_rules' => ['From_str::to_ean13'],
+
+    examples => [
+        {value=>'5-901234-123457', valid=>1, validated_value=>'5901234123457'},
+        {value=>'123-4567890-123', valid=>0, summary=>'Invalid checkdigit'},
+        {value=>'1234567890', valid=>0, summary=>'Less than 13 digits'},
+        {value=>'12345678901234', valid=>0, summary=>'More than 13 digits'},
+    ],
 }, {}];
 
 1;
--- lib/Sah/Schema/ean8.pm
+++ lib/Sah/Schema/ean8.pm
@@ -1,6 +1,8 @@
 package Sah::Schema::ean8;
 
+# AUTHORITY
 # DATE
+# DIST
 # VERSION
 
 our $schema = [str => {
@@ -14,6 +16,13 @@ Checksum digit must be valid.
 _
     match => '\A[0-9]{8}\z',
     'x.perl.coerce_rules' => ['From_str::to_ean8'],
+
+    examples => [
+        {value=>'9638-5074', valid=>1, validated_value=>'96385074'},
+        {value=>'12345678', valid=>0, summary=>'Invalid checkdigit'},
+        {value=>'1234567', valid=>0, summary=>'Less than 8 digits'},
+        {value=>'123456789', valid=>0, summary=>'More than 8 digits'},
+    ],
 }, {}];
 
 1;
